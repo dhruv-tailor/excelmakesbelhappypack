0fa896f53e431629973ca8bc2bebba1738950272
Melle <Mellester1@gmail.com>
2020-04-18 00:15:22 +0200

    Merged arumba chnages

--- common/defines/idea_variation_lite.lua
+++ common/defines/idea_variation_lite.lua
@@ -23,6 +23,6 @@ NDefines.NEconomy.CARAVAN_POWER_MAX = 10
 NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = 0.0
 NDefines.NMilitary.GALLEY_BONUS_INLAND_SEA = 0.0
 NDefines.NMilitary.GALLEY_DICE_MALLUS_HIGH_SEA = -1
-NDefines.NMilitary.NAVAL_BASE_ENGAGEMENT_WIDTH = 60 		-- Number of ships that can fire per round
+NDefines.NMilitary.NAVAL_BASE_ENGAGEMENT_WIDTH = 60 	-- Number of ships that can fire per round
 NDefines.NMilitary.NAVAL_CASUALTY_MIN_MORALE_DAMAGE = 0.0
 NDefines.NMilitary.MERCENARY_SUPPORT_LIMIT_FRACTION = 0.1
