b3d282ff37e33759ba0087230385ae444f62dcb0
Melle <Mellester1@gmail.com>
2020-04-19 16:09:30 +0200

    pyhton changes

--- common/ideas/module1.py
+++ common/ideas/module1.py
@@ -3,6 +3,7 @@ import pathlib
 import os
 import json
 import random
+import localisation
 
 class Ideas:
 	def __init__(self,name, typ, listgroups, trigger):
@@ -15,6 +16,7 @@ def test1():
 	ModFolder = pathlib.PureWindowsPath(r"E:\Melle\Documents\Paradox Interactive\Europa Universalis IV\mod\GitBranch")
 	files = [r"common\ideas\00_admin_ideas.txt" , r"common\ideas\00_dip_ideas.txt", r"common\ideas\00_mil_ideas.txt" ]
 	#files = [f for f in os.listdir('.') if os.path.isfile(f)]
+	loc = localisation.localisation()
 	out = []
 	print (pathlib.Path('.'))
 	for f in files:
@@ -58,32 +60,25 @@ def test1():
 			outfile.write("}\n")
 	outfile.write("}")
 	outfile.close()
-	template = """Add{0} = {{
+	introText = "# this file is genrated by {0} \n".format(os.path.basename(__file__))
+	template ="""Add{0} = {{
 	if = {{ limit = {{ check_variable = {{ which = numOfFreeIdeas value = 1	}} }}
-	if = {{ limit = {{  {1}	  }}
-	add_idea_group = {0}	}}"""
+	if = {{ limit = {{  {2}	  }}
+	add_idea_group = {0}	}} #{1}"""
 	template1 = """	if = {{ limit = {{ check_variable = {{ which = numOfFreeIdeas value = 1	}} }} add_idea = {0}
 		if = {{ limit = {{ has_idea = {0} }} subtract_variable = {{ which = numOfFreeIdeas value = 1 }}	}} }}"""
 	outfile = open("ML_AddIdeas.txt.tmp","w")
+	outfile.write(introText)
 	for ideaG in list:
-		outfile.write(template.format(ideaG.name, PR.encode(ideaG.trigger)) + '\n')
+		outfile.write(template.format(ideaG.name, loc[ideaG.name], PR.encode(ideaG.trigger)) + '\n')
 		for idea in ideaG.listgroups:
 			outfile.write(template1.format(idea) + '\n')
 		outfile.write("}\t }\n")
 	for ideaG in list:
-		outfile.write("#Add{0}\n".format(ideaG.name))
+		outfile.write("#Add{0}".format(ideaG.name).ljust(25," "))
+		outfile.write("{0}\n".format(loc[ideaG.name]))
 	outfile.close()
 	
-	template2 = """	if = {{ limit = {{has_idea_goup = {0} }} add_idea = {0} 
-	"""
-	for itter in range(1,1):
-		random.shuffle(list)
-		outfile = open("ML_ideaorderFirstEffect{0}.txt.tmp".format(itter),"w")
-		outfile.write("ideaorder{0} = {{\n".format(itter))
-		for ideaG in list:			
-			outfile.write("\tAdd{0} = yes\n".format(ideaG.name))
-		outfile.write("}\n")
-		outfile.close()
 	for itter in range(1,6):
 		random.shuffle(list)
 		outfile = open("ML_ideaordereffect{0}.txt.tmp".format(itter),"w")
