0a75cf37974f23a691788fb1ca2a6a6725c58a3b
LoStack <stackpoint@users.noreply.github.com>
2020-04-22 13:30:14 -1000

    April ‎22, ‎2020

---
+++ common/defines/NDefines.NEconomy.lua
@@ -0,0 +1,9 @@
+-- Only Changes to NDefines.NEconomy should be here.
+NDefines.NEconomy.BANKRUPTCY_PROVINCE_DEVASTATION_GAIN = 10
+NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.04
+NDefines.NEconomy.BASE_INTERESTS = 5.0
+NDefines.NEconomy.CARAVAN_FACTOR = 20.0
+NDefines.NEconomy.CARAVAN_POWER_MAX = 10
+NDefines.NEconomy.EDICTS_COST_INCREASE = 1		-- % increase on state maintenance.  *reduced from 200% to 100% to allow more tactical usage of edicts.
+NDefines.NEconomy.EDICTS_DURATION_MONTHS = 3	-- months lasting at least. removed because its annoying to remember managing and being blocked from doing it when you remember.
+NDefines.NEconomy.MAX_BUILDING_SLOTS = 18 
