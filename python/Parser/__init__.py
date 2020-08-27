4673c8f06f3ed10243f4d5a1ca0b017d89a8b78d
afowl <ajnstajner@gmail.com>
2020-08-27 23:31:36 +0200

    Let's start this with UI expanded :D

--- python/Parser/__init__.py
+++
@@ -1,7 +0,0 @@
-from Parser.treeParser import *
-from Parser.database import *
-from Parser.parser import *
-from Parser.IO import *
-from Parser.token import Token
-from Parser.ParserLocalisation import *
-db = database()
