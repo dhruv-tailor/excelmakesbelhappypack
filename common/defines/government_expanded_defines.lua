03856320f1f88983dbcdf0e5732b64843968b2c6
afowl <ajnstajner@gmail.com>
2020-10-22 14:54:33 +0200

    fixing the update of GE and ME

--- common/defines/government_expanded_defines.lua
+++
@@ -1,3 +0,0 @@
-NDefines.NGovernment.GOVERNMENT_REFORM_COST_INCREASE = 25
-NDefines.NCountry.STATE_PRIVILEGE_ADMIN_COST = 1			-- Cost of granting a privilege
-NDefines.NCountry.ESTATE_PRIVILEGES_MAX_CONCURRENT = 8		-- Max. number of privileges active at the same time
